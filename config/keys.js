43c90ca938757db4037e78a7275d9f25e8724bbc
Andriusavlas <andriusavlas@gmail.com>
2018-06-19 17:13:07 +0300

    Authentication with passport example

--- config/keys.js
+++ config/keys.js
@@ -1,3 +1,4 @@
 module.exports = {
-    mongoURI: 'mongodb://andrius:andrius123@ds161610.mlab.com:61610/devconnectorapp'
+    mongoURI: 'mongodb://andrius:andrius123@ds161610.mlab.com:61610/devconnectorapp',
+    secretOrKey: 'secret'
 };
--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,10 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const passport = require('passport');
+
+const keys = require('../../config/keys');
 
 // Load user model
 const User = require('../../models/User');
@@ -44,17 +48,82 @@ router.post('/register', async (req, res) => {
             });
 
             bcrypt.genSalt(10, (err, salt) => {
-                bcrypt.hash(newUser.password, salt, (err, hash) => {
+                bcrypt.hash(newUser.password, salt, async (err, hash) => {
                     if (err) throw err;
                     newUser.password = hash;
+                    await newUser.save();
+                    res.json(newUser);
                 });
             });
-            await newUser.save();
-            res.json(newUser);
         };
     } catch (err) {
         console.log(err);
     };
+});
+
+// @route GET api/users/login
+// @desc Login a user - return a JWT token
+// @access Public 
+
+router.post('/login', async (req, res) => {
+    try {
+        const email = req.body.email;
+        const password = req.body.password;
+
+        // Find user by email
+        const user = await User.findOne({
+            email
+        });
+        if (!user) {
+            return res.status(404).json({
+                email: 'User not found'
+            });
+        };
+        // Check password
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (isMatch) {
+            // User matched
+            // Create JWT payload
+            const payload = {
+                id: user._id,
+                name: user.name,
+                avatar: user.avatar
+            };
+            // Sign token
+            jwt.sign(payload, keys.secretOrKey, {
+                expiresIn: 3600
+            }, (err, token) => {
+                res.json({
+                    success: true,
+                    token: 'Bearer ' + token
+                });
+            });
+        } else {
+            res.status(400).json({
+                password: 'Password incorrect'
+            });
+        };
+    } catch (err) {
+        console.log(err);
+    };
+});
+
+// @route GET api/users/current
+// @desc Return current user
+// @access Private
+router.get('/current', passport.authenticate('jwt', {
+    session: false
+}), (req, res) => {
+    const {
+        id,
+        name,
+        email
+    } = req.user;
+    res.json({
+        id,
+        name,
+        email
+    });
 });
 
 module.exports = router;
