2fdda69621c21cec7886a1eeed07c540790ade9f
Andriusavlas <andriusavlas@gmail.com>
2018-06-21 13:54:24 +0300

    Added profile model and first profile route

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,5 +1,12 @@
 const express = require('express');
 const router = express.Router();
+const mongoose = require('mongoose');
+const passport = require('passport');
+
+// Load Profile model
+const Profile = require('../../models/Profile');
+// Load User model
+const User = require('../../models/User');
 
 // @route GET api/profile/test
 // @desc Tests profile route
@@ -8,6 +15,27 @@ router.get('/test', (req, res) => {
     res.json({
         message: 'Profile works!'
     });
+});
+
+// @route GET api/profile
+// @desc Get current user profile
+// @access Private
+router.get('/', passport.authenticate('jwt', {
+    session: false
+}), async (req, res) => {
+    try {
+        const errors = {};
+        const profile = await Profile.findOne({
+            user: req.user.id
+        });
+        if (!profile) {
+            errors.noprofile = 'There is no profile for this user';
+            return res.status(404).json(errors);
+        };
+        res.json(profile);
+    } catch (err) {
+        res.status(404).json(err);
+    };
 });
 
 module.exports = router;
